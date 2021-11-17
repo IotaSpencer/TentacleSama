49398173af998360422207aed5a6a902a9806b94
Ken Spencer <me@iotaspencer.me>
2021-11-17 03:36:13 -0500

    add check for owner

--- lib/tentacle_sama/helpers.rb
+++ lib/tentacle_sama/helpers.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
-
+require 'tentacle_sama/exceptions'
 module TentacleSama
   module Bot
-    class Helpers
+    module Helpers
+      module_function
 
+      def self.check_for_owner(id)
+        # IotaSpencer's ID
+        user_id = 234093061045616642
+        if event.user.id == TentacleSama::Configs::BotConfig.config.bot.owner.to_i
+
+          rescue TentacleSama::Exceptions::NotOwnerError
+
+        end
+      end
     end
   end
-end
